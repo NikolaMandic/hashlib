339899b04c1aea2b60f30a5ce74da3d6e930dab3
NikolaMandic <mn080202@gmail.com>
2013-11-03 07:27:46 +0100

    dict

--- h.c
+++ h.c
@@ -2,15 +2,28 @@ union{
   float f;
   int i;
 }u;
-int hash(int key){
+#define L 100
+struct Dict{
+ void * val;
+}dict[L];
+int hash(int key,int length){
   u.i=key;
   u.i=u.i&0x3f7fffff;
   u.i=u.i|0x3f000000;
   
-  return u.f*100;
+  return u.f*length;
+}
+int dict_store(struct Dict *d,int(*h)(int,int),int key,void * val,int len){
+   d[h(key,len)].val=val;
 }
 int main(){
   int p=5;
-  printf("%p %d\n",&p,hash((int)&p));
-
+  int i=0;
+  printf("%p %d\n",&p,hash((int)&p,L));
+  char * st="charStr";
+  dict_store(dict,hash,(int)&st,st,L);
+  while(i<100){
+    printf("%d = %p\n",i,dict[i].val);
+    i++;
+  }
 }
